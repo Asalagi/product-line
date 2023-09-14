822698785f86fed81e46d439cbc1bbd148e0fd70
Asalagi <mypaintedfrog@yahoo.com>
2023-09-14 12:58:15 -0500

    fetch request

--- saddlery-project/src/saddles.js
+++ saddlery-project/src/saddles.js
@@ -2,24 +2,38 @@ import './saddlery.css';
 import Header from './header';
 import Footer from './footer';
 import comingsoon from './comingsoon.jpeg'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Saddles() {
+    const [getSaddles, setGetSaddles] = useState([]);
+
+    useEffect(() => {
+        fetchSaddles();
+    }, []);
+
+    const fetchSaddles = () => {
+        axios.get('http://localhost:3001/saddles')
+        .then(res => {
+            console.log({ setGetSaddles: res.data });
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
     return (
         <div className="hb-main-container">     
             <Header></Header>
             <div className="hb-inner-content">
             <div className="inner-content-title">
                 <div className="card-container">
-                    <div className="saddle-card">
+                    {getSaddles.map(getSaddles => (
+                        <div className="saddle-card">
                         <img src={comingsoon} alt="coming soon" height="100"/>
-                        <h3>saddle name</h3>
-                        <p>price</p>
-                    </div>
-                    <div className="saddle-card">
-                        <img src={comingsoon} alt="coming soon" height="100"/>
-                        <h3>saddle name</h3>
-                        <p>price</p>
-                    </div>
+                        <h3>{getSaddles.saddle_name}</h3>
+                        <p>{getSaddles.price}</p>
+                    </div>))}
                 </div>
             </div>
             </div>
