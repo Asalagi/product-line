e09ed58154edd861bce4e041213322196631e43a
Asalagi <mypaintedfrog@yahoo.com>
2023-09-15 14:02:06 -0500

    detail cards

--- saddlery-project/src/saddle-info.js
+++ saddlery-project/src/saddle-info.js
@@ -24,9 +24,21 @@ function SaddleDetails() {
             <div className="hb-inner-content">
             <div className="inner-content-title">
             <h2>{getDetails.saddle_name}</h2>
-            <p>Finding your perfect saddle starts here.</p>
-                <div className="card-container">
-                <img src={comingsoon} alt="coming soon" height="100"/>
+            {getDetails.price}
+                <div className="detail-img-container">
+                <img src={comingsoon} alt="coming soon" height="200"/>
+                <div className="detail-content">
+                    <div className="detail-item">Type </div>
+                    <div className="detail-item">Size</div>
+                    <div className="detail-item">Color</div>
+                    <div className="detail-item">Weight</div>
+                </div>
+                <div className="detail-content">
+                    <div className="detail-item">{getDetails.type}</div>
+                    <div className="detail-item">{getDetails.size}</div>
+                    <div className="detail-item">{getDetails.color}</div>
+                    <div className="detail-item">{getDetails.weight}</div>
+                </div>
                 </div>
             </div>
             </div>
